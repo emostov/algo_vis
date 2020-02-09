480c61f40f1472dc0a9431a750cbbdd30414bd7e
zeke <emostov@middlebury.edu>
2020-02-09 11:10:41 -0800

    board.js corrections

--- src/js/board.js
+++ src/js/board.js
@@ -5,14 +5,14 @@ class Board {
   }
 
   makeRows() {
-    let container = document.getElementsByClassName("board-container")[0]
-    for (let i = 0; i < this.rowLen; i++) {
-      const row = document.createElement("div");
+    const container = document.getElementsByClassName('board-container')[0]
+    for (let i = 0; i < this.rowLen; i += 1) {
+      const row = document.createElement('div');
       row.className = 'grid-row';
       container.appendChild(row);
 
-      for (let j = 0; j < this.colLen; j++) {
-        let cell = document.createElement("div");
+      for (let j = 0; j < this.colLen; j += 1) {
+        const cell = document.createElement('div');
         cell.className = 'grid-item';
         row.appendChild(cell);
         cell.id = `${i}-${j}`;
@@ -21,18 +21,20 @@ class Board {
   }
 
   addSomeTilesClasses() {
-    let classes = ["start-tile", "target-tile", "unvisited-tile", "wall-tile", "shortest-path-tile", "visited-tile"];
+    const classes = ['start-tile', 'target-tile', 'unvisited-tile', 'wall-tile', 'shortest-path-tile', 'visited-tile'];
 
     for (let i = 0; i < this.rowLen; i += 3) {
       for (let j = 0; j < this.colLen; j += 3) {
         const curEl = document.getElementById(`${i}-${j}`);
-        let ranNum = (Math.floor(Math.random() * 10 / 1));
+        const ranNum = (Math.floor((Math.random() * 10) / 1));
         curEl.classList.add(classes[ranNum]);
       }
     }
   }
 }
 
-b1 = new Board(30, 75);
-b1.makeRows();
-b1.addSomeTilesClasses()
+module.exports = Board;
+
+// b1 = new Board(30, 75);
+// b1.makeRows();
+// b1.addSomeTilesClasses()
