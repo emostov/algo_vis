c415f766c71292d4c303114c07e001959f2f1378
Henry Ryu <hkryucr@gmail.com>
2020-02-08 16:51:05 -0800

    finished css parts

--- src/js/board.js
+++ src/js/board.js
@@ -1,46 +1,51 @@
-const container = document.getElementById("container");
+const container = document.getElementsByClassName("board-container")[0];
 
 function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
-        let cell = document.createElement("div");
-        // cell.setAttribute("id", `${row}${col}`)
-        // cell.innerText = (c + 1);
-        container.appendChild(cell).className = 'grid-item';
-        container.appendChild(cell).idName = ''
+    for (i = 0; i < rows; i++) {
+        
+        let row = document.createElement("div");
+        row.className = 'grid-row';
+        container.appendChild(row);
 
+        for (j = 0; j < cols; j++){
+            let cell = document.createElement("div");
+            // cell.setAttribute("id", `${row}${col}`)
+            // cell.innerText = (c + 1);
+            cell.className = 'grid-item';
+            row.appendChild(cell)
+            cell.id = `${i}-${j}`;
+        }
     };
 };
 
-function grid() {
-    let length = 525;
-    let width = 6;
+// function grid() {
+//     let length = 525;
+//     let width = 6;
 
-    let colors = ["red", "orange", "yellow", "green", "blue", "purple"]
-    // for (let i = 0; i < length; i++) {
-    let i = 0;
-    // debugger
-    function loopStep() {
-        if (i === length) {
-            return
-        }
-        setTimeout(function () {
-            // setTimeout(function() {
-            // debugger
-            const tile = document.getElementsByClassName("grid-item")[i]
-            tile.style.backgroundColor = colors[0]
-            // .style.backgroundColor = colors[0]
-            colors.rotateRight(1)
-            loopStep();
-            // }, 0); 
-            i++;
-        }, 50);
-    }
+//     let colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+//     // for (let i = 0; i < length; i++) {
+//     let i = 0;
+//     // debugger
+//     function loopStep() {
+//         if (i === length) {
+//             return
+//         }
+//         setTimeout(function () {
+//             // setTimeout(function() {
+//             // debugger
+//             const tile = document.getElementsByClassName("grid-item")[i]
+//             tile.style.backgroundColor = colors[0]
+//             // .style.backgroundColor = colors[0]
+//             colors.rotateRight(1)
+//             loopStep();
+//             // }, 0); 
+//             i++;
+//         }, 50);
+//     }
 
-    loopStep();
-    // }
-}
+//     loopStep();
+//     // }
+// }
 
 Array.prototype.rotateRight = function (n) {
     for (let i = 0; i < n; i++) {
@@ -50,9 +55,6 @@ Array.prototype.rotateRight = function (n) {
     // return this
 }
 
+makeRows(30, 75);
 
-
-
-makeRows(29, 60);
-
-grid();
+// grid();
